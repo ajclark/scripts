7d6bf585443a4b9a6c415dbba237667fe7c723f7
Allan Clark <napta2k@gmail.com>
2010-08-20 23:25:36 +0100

    Modified comments and usage

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -10,19 +10,19 @@
 # Comment:
 #------------------------------------------------------------------------------
 #       This script can perform a local or remote mysqldump.
-#       This script supports both InnoDB and MyISAM tables
-#       After all databases are dumped they will be compressed, and if remote,
-#       they will be secure copied to a remote host.
+#       This script supports both InnoDB and MyISAM table locking methods
+#       After all databases are dumped they will be compressed and, if remote,
+#       they will be securely copied to a remote host.
 #------------------------------------------------------------------------------
 # Usage:
 #------------------------------------------------------------------------------
 # Loocal mode:
-#  Under local mode all databases will be dumped to a single file and compressed.
+#  All databases will be dumped to a single file and compressed.
 #  local: ./mysql_backup.sh
 #
 # Remote mode:
-#  Under remote mode this script will connect to the remote mysql server and
-#  perform a mysqldump to local disk.
+#  This script will connect to the remote mysql server and
+#  perform a mysqldump to the local server's disk.
 #  remote: ./mysql_backup.sh remote-host.com
 #------------------------------------------------------------------------------
 
@@ -41,7 +41,7 @@ outputDir=/c/home/backups/mysql
 defaultHost=localhost
 lockTables="--lock-all-tables"			# Aquire GLOBAL READ LOCK
 # lockTables="--single-transaction"		# Uncomment for InnoDB tables
-# lockTables=""							# Do not lock tables (DANGEROUS!)
+# lockTables=""							# Do not lock tables (DANGER!)
 outputFile=$(date +%A).sql
 
 #------------------------------------------------------------------------------
