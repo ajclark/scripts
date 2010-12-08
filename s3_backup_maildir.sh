fe10a56b83c7cfe2540518a0afebcdb8de0e898b
Allan Clark <napta2k@gmail.com>
2010-12-08 12:55:31 +0000

    Added exit code, out of boredom

--- s3_backup_maildir.sh
+++ s3_backup_maildir.sh
@@ -2,13 +2,14 @@
 
 # Backup Maildirs to Amazon S3
 # Written by Allan Clark - <napta2k@gmail.com>
+# 2009-07-06
 
 day=$(date +%A).tar.bz2
 bucket="s3://s3.st0len.co.za/"
 
 function s3alert () {
-    mail -s "Backup failure on: ${HOSTNAME}" napta2k@gmail.com
-    exit
+	mail -s "Backup failure on: ${HOSTNAME}" napta2k@gmail.com
+	exit 1
 }
 
 tar -cjf /root/Maildir-${day} /data/Maildir || s3alert
