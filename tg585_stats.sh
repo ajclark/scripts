06185988a40e41a489cb941ba4e7895c8ea270b0
Allan Clark <napta2k@gmail.com>
2010-12-08 15:32:03 +0000

    Looks about finished. Not yet tested with munin.

--- tg585_stats.sh
+++ tg585_stats.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
 
-# Collect stats from TG585 v7 router (lacks snmp)
+# Collect stats from TG585 v7 router via telnet (lacks snmp)
 # Allan Clark - <napta2k@gmail.com>
 # 2010-10-19
 
-# This is currently under a bash script rather than
-# native expect so we can interface with the output
+# Collects a few useful statistics. Note that these stats
+# are just the tip of the ice berg.
 
 # Change "pass" to your router password
 
+if [ $1 == "config" ]; then
+	echo "graph_title TG585 router statistics"
+	echo "graph_category router"
+	echo "connections.label Connections"
+	echo "connections.type GAUGE"
+	exit 0
+fi
+
 # Talk to router over telnet
 expect -c '
-   spawn telnet bebox
-   expect "Username : "
-   send "Administrator\r"
-   expect "Password : "
-   send "pass\r"
-   expect "=>"
-   send ":connection stats\r"
-   expect "=>"
-   send "exit\r"
-   expect eof
-'
+	spawn telnet bebox
+	expect "Username : "
+	send "Administrator\r"
+	expect "Password : "
+	send "pass\r"
+	expect "=>"
+	send ":connection stats\r"
+	expect "=>"
+	send "exit\r"
+	expect eof
+' | awk '/Number of active connections/ { printf "active_connections.value " $NF "\n" }
+	/Number of idle connections/ { printf "idle_connections.value " $NF "\n" }
+	/Number of TCP connections/ { printf "tcp_connections.value " $NF "\n" }
+	/Number of UDP connections/ { printf "udp_connections.value " $NF "\n" }'
